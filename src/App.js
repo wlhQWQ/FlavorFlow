9fde601395b9f6680cf4798d3a2cb1344080f54d
王立恒 <10035239+wangliheng-student@user.noreply.gitee.com>
2026-01-30 16:53:15 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { Layout, Typography } from "antd";
+import { useState } from "react";
+import FoodList from "./components/FoodList";
+import LoginForm from "./components/LoginForm";
+import MyCart from "./components/MyCart";
+import SignupForm from "./components/SignupForm";
+
+const { Header, Content } = Layout;
+const { Title } = Typography;
 
 function App() {
+  const [authed, setAuthed] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Layout style={{ height: "100vh" }}>
+      <Header>
+        <div
+          className="header"
+          style={{ display: "flex", justifyContent: "space-between" }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Title
+            level={2}
+            style={{ color: "white", lineHeight: "inherit", marginBottom: 0 }}
+          >
+            Lai Food
+          </Title>
+          <div>{authed ? <MyCart /> : <SignupForm />}</div>
+        </div>
+      </Header>
+      <Content
+        style={{
+          padding: "50px",
+          maxHeight: "calc(100% - 64px)",
+          overflowY: "auto",
+        }}
+      >
+        {authed ? (
+          <FoodList />
+        ) : (
+          <LoginForm onSuccess={() => setAuthed(true)} />
+        )}
+      </Content>
+    </Layout>
   );
 }
-
 export default App;
---
+++ src/utils.js
@@ -0,0 +1,93 @@
+export const login = (credentials) => {
+  const loginUrl = `/login?username=${credentials.username}&password=${credentials.password}`;
+
+  // fetch will return something called a Promise
+  return fetch(loginUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then((response) => {
+    if (response.status < 200 || response.status >= 300) {
+      throw Error("Fail to log in");
+    }
+  }); // returns a promise
+};
+
+export const signup = (data) => {
+  const signupUrl = "/signup";
+
+  return fetch(signupUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data), // convert a JavaScript object (data) into a JSON string representation.
+  }).then((response) => {
+    if (response.status < 200 || response.status >= 300) {
+      throw Error("Fail to sign up");
+    }
+  });
+};
+
+export const getMenus = (restId) => {
+  return fetch(`/restaurant/${restId}/menu`).then((response) => {
+    if (response.status < 200 || response.status >= 300) {
+      throw Error("Fail to get menus");
+    }
+
+    return response.json(); // reads the response body and parses it as JSON, returns a promise that resolves with the result of parsing the body text as JSON
+  });
+};
+
+
+export const getRestaurants = () => {
+  return fetch("/restaurants/menu").then((response) => {
+    if (response.status < 200 || response.status >= 300) {
+      throw Error("Fail to get restaurants");
+    }
+
+    return response.json();
+  });
+};
+
+export const getCart = () => {
+  return fetch("/cart").then((response) => {
+    if (response.status < 200 || response.status >= 300) {
+      throw Error("Fail to get shopping cart data");
+    }
+
+    return response.json();
+  });
+};
+
+export const checkout = () => {
+  return fetch("/cart/checkout", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then((response) => {
+    if (response.status < 200 || response.status >= 300) {
+      throw Error("Fail to checkout");
+    }
+  });
+};
+
+export const addItemToCart = (itemId) => {
+  const payload = {
+    menu_id: itemId,
+  };
+
+  return fetch(`/cart`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(payload),
+  }).then((response) => {
+    if (response.status < 200 || response.status >= 300) {
+      throw Error("Fail to add menu item to shopping cart");
+    }
+  });
+};
